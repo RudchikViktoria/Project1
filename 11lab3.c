56d5df6025e4932ee9afdcbd2adedeb12b52ec0e
Вика Рудчик <vicha.rudchiik@mail.ru>
2024-09-12 11:55:03 +0300

    Add files via upload

---
+++ 11lab3.c
@@ -0,0 +1,23 @@
+#include <locale.h>
+
+#include <stdio.h>
+
+int main()
+
+{
+	int num1;
+	int num2;
+	setlocale(LC_ALL, "RUS"); 
+	puts("¬ведите число"); 
+	scanf("%d", &num1);
+	scanf("%d", &num2);
+	puts("¬ведите число A");
+	int num
+	scanf("%d", &num);
+	
+
+
+
+
+
+}
