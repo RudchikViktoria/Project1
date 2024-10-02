42d9ef2f7808882cdd8e14b381ee26c4f7184c7e
Вика Рудчик <vicha.rudchiik@mail.ru>
2024-10-02 18:04:35 +0300

    Add files via upload

---
+++ lab6.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <locale.h>
+
+int task()
+{
+	setlocale(LC_CTYPE, "RUS");
+	int a, b, c, max1, max2;
+	printf("Введите число а: ");
+	scanf("%d", &a);
+	printf("Введите число b: ");
+	scanf("%d", &b);
+	printf("Введите число c: ");
+	scanf("%d", &c);
+	if (a > b)
+	{
+		if (a > c)
+		{
+			max1 = a;
+			max2 = b > c ? b : c;
+		}
+		else
+		{
+			max1 = c;
+			max2 = a;
+		}
+	}
+	else
+	{
+		if (b > c)
+		{
+			max1 = b;
+			max2 = a > c ? a : c;
+		}
+		else
+		{
+			max1 = c;
+			max2 = b;
+		}
+
+	}
+	int d = max1 * max2;
+	printf("Произведение двух наибольших чисел равно: %d\n ", d);
+
+
+}
+void main()
+{
+	task();
+}
